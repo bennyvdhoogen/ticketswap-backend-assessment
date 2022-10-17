64ad314f7ff050e931679ae801e1bcbc967a4537
Benny van den Hoogen <bennyvdhoogen@gmail.com>
2022-10-17 21:54:58 +0200

    Added const/message to TicketAlreadySoldException

--- src/Exceptions/TicketAlreadySoldException.php
+++ src/Exceptions/TicketAlreadySoldException.php
@@ -4,6 +4,8 @@ namespace TicketSwap\Assessment\Exceptions;
 
 class TicketAlreadySoldException extends \Exception
 {
+    public const ALREADY_SOLD = "The given ticket has already been sold";
+
     public function __construct()
     {
         //
